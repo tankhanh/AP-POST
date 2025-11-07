f0bd21c78e455a37fe720929c336b33ef890ea5e
AnhMinh <truonghoanganhminh2000@gmail.com>
2025-11-07 23:45:24 +0700

    refactor db

--- BE/src/modules/orders/orders.module.ts
+++ BE/src/modules/orders/orders.module.ts
@@ -1,15 +1,18 @@
 import { Module } from '@nestjs/common';
-import { OrdersService } from './orders.service';
-import { OrdersController } from './orders.controller';
 import { MongooseModule } from '@nestjs/mongoose';
+import { OrdersController } from './orders.controller';
+import { OrdersService } from './orders.service';
 import { Order, OrderSchema } from './schemas/order.schemas';
+import { LocationModule } from '../location/location.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: Order.name, schema: OrderSchema }]),
+    LocationModule,
   ],
   controllers: [OrdersController],
   providers: [OrdersService],
-  exports: [MongooseModule],
+
+  exports: [MongooseModule, OrdersService],
 })
 export class OrdersModule {}
