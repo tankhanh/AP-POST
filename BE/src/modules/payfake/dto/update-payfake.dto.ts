dcb5903d9fb737be2f4e341ee02aefc32ca84813
tankhanh <dinhtankhanh14@gmail.com>
2025-12-07 00:14:18 +0700

    fix

--- BE/src/modules/payfake/dto/update-payfake.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePayfakeDto } from './create-payfake.dto';
-
-export class UpdatePayfakeDto extends PartialType(CreatePayfakeDto) {}
