75e77a0ce44c4a55671a580581208e9cf91b2da9
tankhanh <dinhtankhanh14@gmail.com>
2025-12-10 02:00:48 +0700

    :)

--- FE/src/app/environments/environment.ts
+++ FE/src/app/environments/environment.ts
@@ -2,5 +2,5 @@ export const env = {
     production: true,
     baseUrl: 'https://ap-post-api.onrender.com/api/v1',
 
-    fake_payment: 'https://fake-payment-gateway.vercel.app/api'
+    fake_payment: 'https://fake-payment-gateway.vercel.app/api/v1'
 }
